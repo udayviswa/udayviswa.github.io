76d73bf9526692ac8870a531cc4a5852bab9e72d
udayviswa <udayviswa23@gmail.com>
2023-11-25 22:41:18 +0530

    v3

--- MyCartoons/script.js
+++ MyCartoons/script.js
@@ -8,8 +8,8 @@ const movies = [
     },
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
-    imagePath: "totoro.jpg",
-    movieName: "My Neighbour Totoro"
+    imagePath: "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
+    movieName: "Doraemon"
     },
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
@@ -40,6 +40,9 @@ const movies = [
 ]
 
 // need to implement better version of loop and variable names
+// <p class="movieNames">${movie.movieName}</p> 
+// <button type="button" class="movieNames">${movie.movieName}</button>
+
 var index = 0; 
 // var listItems = "";
 var listItems = "";
@@ -48,7 +51,7 @@ while(index < movies.length){
   listItems += `<div class="cards" id="cards">
       <a href= ${movie.movieLink}>
       <img src= ${movie.imagePath} alt="">
-      <p class="movieNames">${movie.movieName}</p>
+      <p class="movieNames" id="movieNames">${movie.movieName}</p> 
       </a>
       </div>`;
    index++;
@@ -57,12 +60,30 @@ document.getElementById("container").innerHTML = listItems;
 
 // dark mode code 
 function darkMode(){
-    document.body.style.backgroundColor = "black";
-    document.getElementById("darkMode").style.backgroundColor = "black";
-    // left at p element color change 
-    
-    // element.querySelectorAll(".movieNames").style.backgroundColor = "black";
-    // const movieName = document.getElementById("movieNames");
-    // movieName.style.backgroundColor = "black";
-    // document.getElementsByTagName("P").style.backgroundColor = "black";
+    let color1 = document.getElementById("darkMode").innerText;
+    if ( color1 == "Light"){
+      lightMode();
+    }
+    else{
+      dark();
+    }
+        // document.getElementById('myImage').src='pic_bulbon.gif'"
 }
+
+function dark(){
+  document.body.style.backgroundColor = "black";
+  document.getElementById("darkMode").style.backgroundColor = "white";
+  $(".movieNames").css("background-color", "black");
+  $("#header").css("background-color", "black");
+  document.getElementById("darkMode").innerText = "Light";
+}
+
+function lightMode(){
+    document.body.style.backgroundColor = "white";
+    document.getElementById("darkMode").style.backgroundColor = "white";
+    $(".movieNames").css("background-color", "teal");
+    $(".movieNames").css("color", "white");
+    $("#header").css("background-color", "teal");
+    document.getElementById("darkMode").innerText = "Dark";
+}
+

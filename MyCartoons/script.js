f305adff75976582ada79e340dd9ade8ffb4b96e
udayviswa <udayviswa23@gmail.com>
2023-11-26 01:39:12 +0530

    v3

--- MyCartoons/script.js
+++ MyCartoons/script.js
@@ -4,37 +4,44 @@ const movies = [
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
     imagePath: "totoro2.jpg",
-    movieName: "My Neighbour Totoro"
+    movieName: "My Neighbour Totoro",
+    genre:"cartoon"
     },
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
     imagePath: "https://m.media-amazon.com/images/M/MV5BNTkzNjJhYjQtNjU0Yy00Y2M3LWI2ZDgtNDRhZmNlNDFjMjQ5XkEyXkFqcGdeQXVyODk2ODI3MTU@._V1_FMjpg_UX1000_.jpg",
-    movieName: "Doraemon"
+    movieName: "Doraemon",
+    genre:"cartoon"
     },
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
     imagePath: "totoro2.jpg",
-    movieName: "My Neighbour Totoro"
+    movieName: "My Neighbour Totoro",
+    genre:"movie"
     },
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
     imagePath: "totoro.jpg",
-    movieName: "My Neighbour Totoro"
+    movieName: "My Neighbour Totoro",
+    genre:"serial"
     },
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
     imagePath: "totoro.jpg",
-    movieName: "My Neighbour Totoro"
+    movieName: "My Neighbour Totoro",
+    genre:"movie"
     },
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
     imagePath: "totoro.jpg",
-    movieName: "My Neighbour Totoro"
+    movieName: "My Neighbour Totoro",
+    genre:"serial"
     },
     {
     movieLink: "https://archive.org/details/totoro-foxdub",
     imagePath: "totoro.jpg",
-    movieName: "My Neighbour Totoro"
+    movieName: "My Neighbour Totoro",
+    genre:"movie"
     }
 
 ]
@@ -48,7 +55,7 @@ var index = 0;
 var listItems = "";
 while(index < movies.length){
   var movie = movies[index];
-  listItems += `<div class="cards" id="cards">
+  listItems += `<div class="cards ${movie.genre}" id="cards">
       <a href= ${movie.movieLink}>
       <img src= ${movie.imagePath} alt="">
       <p class="movieNames" id="movieNames">${movie.movieName}</p> 
@@ -67,7 +74,6 @@ function darkMode(){
     else{
       dark();
     }
-        // document.getElementById('myImage').src='pic_bulbon.gif'"
 }
 
 function dark(){
@@ -75,6 +81,7 @@ function dark(){
   document.getElementById("darkMode").style.backgroundColor = "white";
   $(".movieNames").css("background-color", "black");
   $("#header").css("background-color", "black");
+  // $(".filter-item").css("color", "white");
   document.getElementById("darkMode").innerText = "Light";
 }
 
@@ -84,6 +91,29 @@ function lightMode(){
     $(".movieNames").css("background-color", "teal");
     $(".movieNames").css("color", "white");
     $("#header").css("background-color", "teal");
+    // $(".filter-item").css("color", "teal");
+    // $(".active-filter").css("background-color", "teal");
+    // $(".active-filter").css("color", "white");
     document.getElementById("darkMode").innerText = "Dark";
 }
 
+// filter function 
+
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+      const value = $(this).attr("data-filter");
+      if (value == "all"){
+          $(".cards").show("10000")
+      } else{
+          $(".cards")
+              .not("." + value)
+              .hide(1);
+          $(".cards")
+          .filter("." + value)
+          .show("3000")
+      }
+  });
+  $(".filter-item").click(function () {
+      $(this).addClass("active-filter").siblings().removeClass("active-filter")
+  });
+});

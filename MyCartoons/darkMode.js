a3d896e93666ddd8ac9ef8282b10e949a26b25c2
udayviswa <udayviswa23@gmail.com>
2024-01-03 22:59:33 +0530

    v2

--- MyCartoons/darkMode.js
+++ MyCartoons/darkMode.js
@@ -1,27 +1,50 @@
 // dark mode code 
-function darkMode(){
-    let color1 = document.getElementById("darkMode").innerText;
-    if ( color1 == "Light"){
-      lightMode();
-    }
-    else{
-      dark();
-    }
-}
+// function darkMode(){
+//     let color1 = document.getElementById("darkMode").innerText;
+//     if ( color1 == "Light"){
+//       lightMode();
+//     }
+//     else{
+//       dark();
+//     }
+// }
+
+// function darkmodeCheck(){
+//   // let check = document.body.style.backgroundColor;
+//   if (document.body.style.backgroundColor != "white"){
+//     dark();
+//   }else {
+//   }
+// }
+// localStorage.setItem("mode", "dark");
+
+
 
 function dark(){
-  document.body.style.backgroundColor = "black";
-  document.getElementById("darkMode").style.backgroundColor = "white";
-  $(".movieNames").css("background-color", "black");
-  $("#header").css("background-color", "black");
-  document.getElementById("darkMode").innerText = "Light";
+  let bodyColor = document.body;
+  bodyColor.classList.toggle("darkmodeOn");
+
+  let movieNames = document.getElementsByTagName("p");
+  console.log(movieNames);
+  for (let index of movieNames){
+    index.classList.toggle("darkmodeOn");
+  }
+
+  let headerColor = document.getElementById("header");
+  headerColor.classList.toggle("darkmodeOn");
+  
+  // document.body.style.backgroundColor = "black";
+  // document.getElementById("darkMode").style.backgroundColor = "white";
+  // $(".movieNames").css("background-color", "black");
+  // $("#header").css("background-color", "black");
+  // document.getElementById("darkMode").innerText = "Light";
 }
 
-function lightMode(){
-    document.body.style.backgroundColor = "white";
-    document.getElementById("darkMode").style.backgroundColor = "white";
-    $(".movieNames").css("background-color", "teal");
-    $(".movieNames").css("color", "white");
-    $("#header").css("background-color", "teal");
-    document.getElementById("darkMode").innerText = "Dark";
-}
+// function lightMode(){
+//     document.body.style.backgroundColor = "white";
+//     document.getElementById("darkMode").style.backgroundColor = "white";
+//     $(".movieNames").css("background-color", "teal");
+//     $(".movieNames").css("color", "white");
+//     $("#header").css("background-color", "teal");
+//     document.getElementById("darkMode").innerText = "Dark";
+// }
